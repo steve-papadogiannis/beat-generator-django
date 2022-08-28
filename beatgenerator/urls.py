ceadbb9df28ab7dcd6f145eb010896e74b3703c8
Stefanos Papadogiannis <steve.papadogiannis1992@gmail.com>
2022-08-28 18:17:33 +0300

    Adding django project skeleton

--- beatgenerator/urls.py
+++ beatgenerator/urls.py
@@ -1,4 +1,4 @@
-"""BeatGeneratorDjango URL Configuration
+"""beatgenerator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,17 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from BeatGeneratorDjango.quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
